288904c5b01c2d9586aa97b57a8529954bb7637b
unknown <you@example.com>
2024-04-23 09:48:06 +0700

    menambahkan login.php

--- login.php
+++ login.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar git</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Belajar git pa rifki</h1>
-    <p>ini adalah belajar git</p>
-    <footer>saya pergi ke sekolah</footer>
+    
 </body>
 </html>
